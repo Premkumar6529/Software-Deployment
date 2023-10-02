0daccc4d94832f440d461392bb116711f600cdc4
Premkumar6529 <pkarre@my.okcu.edu>
2023-10-01 22:31:51 -0500

    Changes modified to A+, B+, C+, D+

---
+++ LetterGrade.java
@@ -0,0 +1,62 @@
+// Group Name : Team Prem and Aditya --- Leader : Prem
+
+import java.util.Scanner;
+public class LetterGrade
+{
+    public static void main(String[] args)
+    {
+        String lettergrade;
+
+        System.out.println("Please enter your grade : ");
+        Scanner scan = new Scanner(System.in);
+
+        int grade =  scan.nextInt();
+
+        // if grade >= 90, set the letterGrade to A
+
+        if (grade >= 95){
+        lettergrade = "A+";
+        }
+
+        else if(grade >=90){
+            lettergrade = "A";
+        }
+
+        // if grade >=80 set the letterGrade to B
+
+        else if (grade >= 85){
+        lettergrade = "B+";
+        }
+
+         else if (grade >= 80){
+        lettergrade = "B";
+        }
+
+        // if grade >=70 set the letterGrade to C
+
+        else if (grade >= 75){
+        lettergrade = "C+";
+        }
+
+        else if (grade >= 70){
+        lettergrade = "C";
+        }
+
+        // if grade >=60 set the letterGrade to D
+
+        else if (grade >= 65){
+        lettergrade = "D+";
+        }
+
+        else if (grade >= 60){
+        lettergrade = "D";
+        }
+
+        //else set the letterGrade to F
+
+        else {
+        lettergrade ="F";
+        }
+            System.out.println("The Final Grade is : "+ lettergrade);
+    }//main
+}
